fcb9b78fc46d2e75f7e6872a017e4083b04cd950
ivyscarlett-tech <ivy.o.ansah@gmail.com>
2025-11-10 20:37:50 +0000

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "f84focad66b46c3637d8a9tf61e1920f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -17,3 +32,5 @@ document.addEventListener("DOMContentLoaded", function () {
     );
   }
 });
+
+searchCity("London");
